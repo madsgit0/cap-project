920fcbfe63a5d8df6f97601528f9135230539aba
madsgit0 <landolfomattia@gmail.com>
2022-12-07 16:05:14 +0100

    autenticazione-fix

--- notbored/src/app/auth/auth.guard.ts
+++ notbored/src/app/auth/auth.guard.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor (private authSevice: AuthService) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authSevice.getIsLogin();
   }
 
 }
--- notbored/src/app/auth/auth.service.ts
+++ notbored/src/app/auth/auth.service.ts
@@ -1,20 +1,55 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { RegisterAuth } from './register-auth';
+
+export interface AuthData {
+  accessToken: string;
+  user: {
+    firstname: string;
+    lastname: string;
+    email: string;
+    id: number
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http:HttpClient) { }
+  private isLogin = true;
+
+  constructor(private http:HttpClient) {
+    this.restore();
+  }
+
+  restore() {
+    const userLogin = localStorage.getItem('userLogin');
+    if(userLogin){
+      this.isLogin = true;
+    }else{
+      this.isLogin = false;
+    }
+  }
 
   signup(obj: RegisterAuth){
     return this.http.post(environment.urlAPI + 'register', obj)
   }
 
   signin(obj: RegisterAuth){
-    return this.http.post(environment.urlAPI + 'login', obj)
+    return this.http.post<AuthData>(environment.urlAPI + 'login', obj).pipe(
+      tap((data) => {
+        console.log(data)
+        this.isLogin = true;
+        console.log(this.isLogin)
+      })
+    )
   }
+
+  getIsLogin(){
+    return this.isLogin
+  }
+
 }
